1d42cc97c4ab2ba971c0ecd72c1feea3416af468
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-11-24 22:37:02 +0000

    Add hover lines :-)

--- src/components/Cfd.tsx
+++ src/components/Cfd.tsx
@@ -7,7 +7,8 @@ https://www.mattlayman.com/blog/2015/d3js-area-chart/
 https://bl.ocks.org/d3noob/119a138ef9bd1d8f0a8d57ea72355252
 https://medium.com/@louisemoxy/how-to-create-a-stacked-area-chart-with-d3-28a2fee0b8ca
 
-Hover behaviour / tooltip
+Hover lines / tooltip
+https://bl.ocks.org/alandunning/cfb7dcd7951826b9eacd54f0647f48d3
 https://medium.com/@louisemoxy/create-an-accurate-tooltip-for-a-d3-area-chart-bf59783f8a2d
 
 Responsiveness
@@ -36,6 +37,8 @@ interface StackDatum {
 const color = ['lightgreen', 'lightblue', 'pink']
 const strokeWidth = 1.5
 
+const bisectDate = d3.bisector((d: StackDatum) => d.timestamp).left
+
 const Cfd: React.FunctionComponent<Props> = ({ points }) => {
   const d3Container = useRef(null)
 
@@ -58,8 +61,21 @@ const Cfd: React.FunctionComponent<Props> = ({ points }) => {
       })
 
       const margin = { top: 20, right: 20, bottom: 30, left: 50 }
-      const width = 575 - margin.left - margin.right
-      const height = 350 - margin.top - margin.bottom
+      const svg = d3.select(d3Container.current)
+      // .attr('width', width + margin.left + margin.right)
+      // .attr('height', height + margin.top + margin.bottom)
+
+      const width = +svg.attr('width') - margin.left - margin.right
+      const height = +svg.attr('height') - margin.top - margin.bottom
+      // const width = 575 - margin.left - margin.right
+      // const height = 350 - margin.top - margin.bottom
+
+      // Sometimes called g in bl.ocks.org examples
+      const chart = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`).attr('class', 'chart')
+      const grp = chart
+        .append('g')
+        .attr('transform', `translate(-${margin.left - strokeWidth},0)`)
+        .attr('class', 'grp')
 
       const xDomain = d3.extent(stackedData[stackedData.length - 1].map((sd) => sd.timestamp)) as [number, number]
       const xScale = d3.scaleLinear().domain(xDomain).range([0, width])
@@ -76,14 +92,9 @@ const Cfd: React.FunctionComponent<Props> = ({ points }) => {
         .y0((d) => yScale(d.values[0]))
         .y1((d) => yScale(d.values[1]))
 
-      const svg = d3
-        .select(d3Container.current)
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-      const chart = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`)
-      const grp = chart.append('g').attr('transform', `translate(-${margin.left - strokeWidth},-${margin.top})`)
       const series = grp.selectAll('.series').data(stackedData).enter().append('g').attr('class', 'series')
 
+      // TODO: Use CSS (we can even have multiple styles using https://github.com/theBstar/react-switch-theme or similar)
       series
         .append('path')
         .attr('transform', `translate(${margin.left},0)`)
@@ -93,6 +104,9 @@ const Cfd: React.FunctionComponent<Props> = ({ points }) => {
         .attr('stroke-linecap', 'round')
         .attr('stroke-width', strokeWidth)
         .attr('d', (d) => area(d))
+        .on('mousemove', (a, b) => {
+          // console.log('DATUM', b)
+        })
 
       // Add the X Axis
       chart
@@ -103,10 +117,65 @@ const Cfd: React.FunctionComponent<Props> = ({ points }) => {
 
       // Add the Y Axis
       chart.append('g').attr('class', 'y axis').attr('transform', `translate(0, 0)`).call(yAxis)
+
+      // Focus lines
+      const focus = chart.append('g').attr('class', 'focus').style('display', 'none')
+
+      focus.append('line').attr('class', 'x-hover-line hover-line').attr('y1', 0).attr('y2', height)
+      focus.append('line').attr('class', 'y-hover-line hover-line').attr('x1', width).attr('x2', width)
+
+      focus.append('circle').attr('r', 7.5)
+
+      focus.append('text').attr('x', 15).attr('dy', '.31em')
+
+      svg
+        .append('rect')
+        .attr('transform', `translate(${margin.left},${margin.top})`)
+        .attr('class', 'overlay')
+        .attr('width', width)
+        .attr('height', height)
+        .on('mouseover', () => focus.style('display', null))
+        .on('mouseout', () => focus.style('display', 'none'))
+        .on('mousemove', (evt) => {
+          const data = stackedData[stackedData.length - 1]
+          const pointer = d3.pointer(evt)
+
+          const x0 = xScale.invert(pointer[0]),
+            i = bisectDate(data, x0, 1),
+            d0 = data[i - 1],
+            d1 = data[i],
+            d = x0 - d0.timestamp > d1.timestamp - x0 ? d1 : d0
+          const translateX = xScale(d.timestamp)
+          const translateY = yScale(d.values[0])
+          focus.attr('transform', `translate(${translateX},${translateY})`)
+          focus.select('text').text(() => d.values[1])
+          focus.select('.x-hover-line').attr('y2', height - translateY)
+          focus.select('.y-hover-line').attr('x2', width + width)
+        })
+      //
+      // focus.append('line').attr('class', 'y-hover-line hover-line').attr('x1', width).attr('x2', width)
+      //
+      // focus.append('circle').attr('r', 7.5)
+      //
+      // focus.append('text').attr('x', 15).attr('dy', '.31em')
+      //
+      // svg
+      //   .append('rect')
+      //   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+      //   .attr('class', 'overlay')
+      //   .attr('width', width)
+      //   .attr('height', height)
+      //   .on('mouseover', function () {
+      //     focus.style('display', null)
+      //   })
+      //   .on('mouseout', function () {
+      //     focus.style('display', 'none')
+      //   })
+      // .on('mousemove', mousemove)
     }
   }, [points])
 
-  return <svg width={400} height={200} ref={d3Container} />
+  return <svg width={800} height={400} ref={d3Container} />
 }
 
 export default Cfd
